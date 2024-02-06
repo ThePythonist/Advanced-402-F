a4c270261ded9e50e6b5462d755f9b8cc6468ade
Arash <53490252+ThePythonist@users.noreply.github.com>
2024-02-06 10:59:35 +0330

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+con = sqlite3.connect('info.db')
+cur = con.cursor()
+
+# cur.execute("CREATE TABLE IF NOT EXISTS customers (name, city, phone);")
+# cur.execute("INSERT INTO customers VALUES ('Mohsen','Tehran','09213378325');")
+# cur.execute("INSERT INTO customers VALUES ('Yas','Shiraz','09334458378');")
+# cur.execute("INSERT INTO customers VALUES ('Mohammad','Tabriz','09193378444');")
+# cur.execute("INSERT INTO customers VALUES ('Mohsen','Tehran','09213370909');")
+
+def select1():
+    cur.execute("SELECT * FROM customers;")
+    records = cur.fetchall()
+    for i in records:
+        print(i[-1])
+
+def select2():
+    cur.execute("SELECT phone FROM customers;")
+    records = cur.fetchall()
+    for i in records:
+        print(i[0])
+
+select1() # OR select2()
+
+con.commit()
+con.close()
+print('Done')
